9689f8d92709af7ecb4cf9f51ef673698d9e6ecd
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-09-14 16:29:25 -0400

    Mount project recovery status control

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import WorkspaceShell from './WorkspaceShell'
 import AuthorCaptureOverlay from './AuthorCaptureOverlay'
+import ProjectRecoveryStatus from './ProjectRecoveryStatus'
 import StableDiffusionStatus from './StableDiffusionStatus'
 import VoiceFingerprintOverlay from './VoiceFingerprintOverlay'
 import WriteWorkspacePro from './WriteWorkspacePro'
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
     <WriteWorkspacePro />
     <AuthorCaptureOverlay />
     <VoiceFingerprintOverlay />
+    <ProjectRecoveryStatus />
     <StableDiffusionStatus />
   </StrictMode>,
 )
