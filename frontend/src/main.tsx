e99579ffdd5b07b6f6c45f364209883c34802f3f
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-09-15 10:38:52 -0400

    Gate startup on restored local model configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import WorkspaceShell from './WorkspaceShell'
 import AuthorCaptureOverlay from './AuthorCaptureOverlay'
 import ProjectRecoveryStatus from './ProjectRecoveryStatus'
-import StableDiffusionStatus from './StableDiffusionStatus'
+import StartupModelGuard from './StartupModelGuard'
 import VoiceFingerprintOverlay from './VoiceFingerprintOverlay'
 import WriteWorkspacePro from './WriteWorkspacePro'
 import './folder-picker-directory'
@@ -15,11 +15,12 @@ import './write-workspace-header.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WorkspaceShell />
-    <WriteWorkspacePro />
-    <AuthorCaptureOverlay />
-    <VoiceFingerprintOverlay />
-    <ProjectRecoveryStatus />
-    <StableDiffusionStatus />
+    <StartupModelGuard>
+      <WorkspaceShell />
+      <WriteWorkspacePro />
+      <AuthorCaptureOverlay />
+      <VoiceFingerprintOverlay />
+      <ProjectRecoveryStatus />
+    </StartupModelGuard>
   </StrictMode>,
 )
