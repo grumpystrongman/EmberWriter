7af676516fb86352302d6375950d106384bcbe30
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-09-17 11:13:41 -0400

    Start creative model provisioning with backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,8 @@
 
 from .generation_reliability import install_generation_reliability
 from .generation_reliability_refinement import install_refinement
+from .model_provisioning import start_creative_model_provisioning
 
 install_generation_reliability()
 install_refinement()
+start_creative_model_provisioning()
