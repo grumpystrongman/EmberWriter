b1d78986f35e1db026ff8d1573c3c11de1a5a833
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-09-17 13:53:56 -0400

    Install explicitness enforcement

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 """EmberWriter backend package initialization."""
 
+from .explicitness_enforcement import install_explicitness_enforcement
 from .generation_reliability import install_generation_reliability
 from .generation_reliability_refinement import install_refinement
 from .model_provisioning import start_creative_model_provisioning
 
 install_generation_reliability()
 install_refinement()
+install_explicitness_enforcement()
 start_creative_model_provisioning()
